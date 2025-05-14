11769be60e414f216d046ee27d2dcba85a7848ff
José_Viniciusq <viniciusa.s.queiroz@gmail.com>
2025-05-14 08:20:45 -0300

    testando

---
+++ src/StockMarket/Broker.java
@@ -0,0 +1,7 @@
+package StockMarket;
+
+import jade.core.Agent;
+
+public class Broker extends Agent {
+
+}
---
+++ src/StockMarket/Investor.java
@@ -0,0 +1,7 @@
+package StockMarket;
+
+import jade.core.Agent;
+
+public class Investor extends Agent {
+
+}
