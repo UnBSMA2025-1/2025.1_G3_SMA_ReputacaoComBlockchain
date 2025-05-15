762b44159b14b19cc7064a74a3d9df65aa6dc12a
José_Viniciusq <viniciusa.s.queiroz@gmail.com>
2025-05-15 17:36:42 -0300

    Adicionando precos nas acoes

--- src/StockMarket/Broker.java
+++ src/StockMarket/Broker.java
@@ -1,7 +1,85 @@
 package StockMarket;
 
-import jade.core.Agent;
+//library
+import jade.core.Agent; 
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;                    
+import jade.lang.acl.ACLMessage;                                
+import jade.domain.DFService;                        
+import jade.domain.FIPAException;                    
+import jade.domain.FIPAAgentManagement.DFAgentDescription; 
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+import java.util.*;
 
 public class Broker extends Agent {
-
+	
+	//prices table and random
+	private Map<String, Double> pricestable = new HashMap<>();
+	private Random random = new Random();
+	
+	
+	@Override
+	protected void setup() {
+		// start message 
+		System.out.println(getLocalName() + "Start broker" );
+		
+		//actions
+		String[] actions = {"BBDC3","AZUL4","CRFB3","ABEV3"};
+		
+		//Generating prices
+		for (String action:actions) {
+			double price = generateStockPrices(action);
+			pricestable.put(action, price);
+			
+			//Register in DF			
+			try {
+				DFAgentDescription dfd = new  DFAgentDescription();
+				dfd.setName(getAID());
+				
+				ServiceDescription sd = new ServiceDescription();
+				sd.setType(action);
+				
+				dfd.addServices(sd);
+				DFService.register(this, dfd);
+			}
+			catch (FIPAException e) {
+                e.printStackTrace();
+            }								
+			
+		}
+		
+		System.out.println(getLocalName() + "actions created");
+        for (Map.Entry<String, Double> brokeractions : pricestable.entrySet()) {
+            System.out.println(" - " + brokeractions.getKey() + ": R$ " + brokeractions.getValue());
+        }
+				
+	}
+	
+	private double generateStockPrices(String action) {
+		double base = 5 + random.nextDouble() * 195; // base value
+		double reflexivity = random.nextDouble() * 0.05; //expectations
+		double transaction = 0.001 + random.nextDouble() * 0.004; //broker fee
+		
+		base *= 1 + (reflexivity *(random.nextGaussian() / 10));
+		if (base < 0.01) {
+			base = 0.01;
+		}
+		double finalprice = base * (1 + transaction);
+		
+		return Math.round(finalprice * 100.0) / 100.0;		
+	}
+	
+	@Override
+	protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+        System.out.println(getLocalName() + "closing broker");
+		
+	}
+	
 }
