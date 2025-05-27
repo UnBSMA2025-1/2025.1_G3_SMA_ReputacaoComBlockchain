6c45610c118cab2dac58f6f0d6d599afdb2aef59
tegnoue <tegnoue@outlook.com>
2025-05-26 22:47:39 -0300

    fix: change some functions names

--- src/StockMarket/GlobalReputationManager.java
+++ src/StockMarket/GlobalReputationManager.java
@@ -7,14 +7,14 @@ import java.util.Collections;
 
 public class GlobalReputationManager {
 
-    private static final Map<AID, AgentReputationData> reputationDatabase = new HashMap<>();
+    private static final Map<AID, ReputationCalculator> reputationDatabase = new HashMap<>();
 
     public static synchronized void recordInteractionOutcome(AID agentAID, boolean wasSuccessful) {
     	
-        reputationDatabase.putIfAbsent(agentAID, new AgentReputationData(agentAID.getLocalName()));
+        reputationDatabase.putIfAbsent(agentAID, new ReputationCalculator(agentAID.getLocalName()));
         
         // Get the reputation data for the agent and record the outcome on the db
-        AgentReputationData rData = reputationDatabase.get(agentAID);
+        ReputationCalculator rData = reputationDatabase.get(agentAID);
         rData.recordInteraction(wasSuccessful);
 
         // for debug purposes
@@ -23,7 +23,7 @@ public class GlobalReputationManager {
 
     // returns the reputation score of the agent
     public static synchronized double getAgentReputation(AID agentAID) {
-        AgentReputationData rData = reputationDatabase.get(agentAID);
+        ReputationCalculator rData = reputationDatabase.get(agentAID);
         if (rData == null) {
             return 0.5; 
         }
@@ -37,7 +37,7 @@ public class GlobalReputationManager {
             return;
         }
         System.out.println("\n----- CURRENT GLOBAL REPUTATIONS -----");
-        for (Map.Entry<AID, AgentReputationData> entry : reputationDatabase.entrySet()) {
+        for (Map.Entry<AID, ReputationCalculator> entry : reputationDatabase.entrySet()) {
             System.out.println(entry.getKey().getLocalName() + ": " + entry.getValue().getReputationScore());
         }
         System.out.println("-------------------------------------\n");
